223304cd4df4d57c66f0e9c834b857878cdfa088
bannzai <kingkonog999yhirose@gmail.com>
2020-12-10 00:55:50 +0900

    :sparkles: initial commit

---
+++ Toilet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Toilet
+//
+//  Created by Yudai.Hirose on 2020/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(action: {
+                
+            }, label: {
+                Text("Toilet")
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Toilet/ToiletApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToiletApp.swift
+//  Toilet
+//
+//  Created by Yudai.Hirose on 2020/12/10.
+//
+
+import SwiftUI
+
+@main
+struct ToiletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
